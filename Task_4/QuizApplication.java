3e3fca93a62c7d6eb757aaa0fb58f44811a8df45
tanya kumari <88139547+tanyakumari18@users.noreply.github.com>
2024-03-28 13:26:07 +0530

    first commit

---
+++ Task_4/QuizApplication.java
@@ -0,0 +1,141 @@
+package Task_4;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
+public class QuizApplication extends JFrame {
+    private JLabel questionLabel;
+    private JRadioButton[] options;
+    private JButton submitButton;
+    private JLabel timerLabel; // Add timer label
+    private int currentQuestionIndex;
+    private int score;
+    private Timer timer;
+
+    private String[] questions = {
+            "Which planet has the most moons?",
+            "What is 2 + 2?",
+            "What is the largest planet in our solar system?"
+    };
+    private String[][] choices = {
+            { "Saturn", "Star", "Sun", "Moon" },
+            { "3", "4", "5", "6" },
+            { "Earth", "Jupiter", "Mars", "Venus" }
+    };
+    private int[] correctAnswers = { 1, 2, 3 };
+
+    public QuizApplication() {
+        setTitle("Quiz Application");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(600, 300);
+        setLayout(new BorderLayout());
+
+        questionLabel = new JLabel();
+        add(questionLabel, BorderLayout.NORTH);
+
+        JPanel optionsPanel = new JPanel(new GridLayout(4, 2));
+        options = new JRadioButton[4];
+        ButtonGroup buttonGroup = new ButtonGroup();
+        for (int i = 0; i < 4; i++) {
+            options[i] = new JRadioButton();
+            options[i].setText(choices[currentQuestionIndex][i]);
+            buttonGroup.add(options[i]);
+            optionsPanel.add(options[i]);
+        }
+        add(optionsPanel, BorderLayout.CENTER);
+
+        submitButton = new JButton("Submit");
+        add(submitButton, BorderLayout.SOUTH);
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkAnswer();
+                showNextQuestion();
+            }
+        });
+
+        timerLabel = new JLabel();
+        add(timerLabel, BorderLayout.EAST); // Add the timer label
+
+        currentQuestionIndex = 0;
+        showQuestion();
+        startTimer();
+    }
+
+    private void showQuestion() {
+        questionLabel.setText(questions[currentQuestionIndex]);
+        for (int i = 0; i < 4; i++) {
+            options[i].setText(choices[currentQuestionIndex][i]);
+            options[i].setSelected(false);
+        }
+    }
+
+    private void showNextQuestion() {
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length) {
+            showQuestion();
+            resetTimer();
+        } else {
+            showResult();
+        }
+    }
+
+    private void checkAnswer() {
+        for (int i = 0; i < 4; i++) {
+            if (options[i].isSelected() && i == correctAnswers[currentQuestionIndex]) {
+                score++;
+            }
+        }
+    }
+
+    private void showResult() {
+        JOptionPane.showMessageDialog(this, "Quiz Completed!\nScore: " + score + "/" + questions.length);
+        System.exit(0);
+    }
+
+    private void startTimer() {
+        timer = new Timer(1000, new ActionListener() { // Timer ticks every 1 second
+            private int secondsLeft = 30; // Initial timer value
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (secondsLeft >= 0) {
+                    timerLabel.setText("Time left: " + secondsLeft + " seconds");
+                    secondsLeft--;
+                } else {
+                    timer.stop();
+                    checkAnswer();
+                    showNextQuestion();
+                }
+            }
+        });
+        timer.start();
+    }
+
+    private void resetTimer() {
+        timer.stop();
+        startTimer();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new QuizApplication().setVisible(true);
+            }
+        });
+    }
+}
